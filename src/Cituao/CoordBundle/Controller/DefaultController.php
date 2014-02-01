3b416046fa6acf09b2bcf671b5f5356e4fa20d13
Jesus Marquez <jesmarquezacevedo@yahoo.es>
2014-02-01 00:13:04 -0500

    Paso de parametros-Ciclos en Twig

--- src/Cituao/CoordBundle/Controller/DefaultController.php
+++ src/Cituao/CoordBundle/Controller/DefaultController.php
@@ -13,15 +13,16 @@ class DefaultController extends Controller
 	
 	public function practicantesAction(){
 		
-		$practicante = array("nombres"=>"JESUS ALBERTO", "apellidos" => "MARQUEZ ACEVEDO", "cedula" => "12502219");
-		return $this->render('CituaoCoordBundle:Default:practicantes.html.twig');
+		$listaPracticantes = array( array("asignatura"=>"CS05", "codigo"=>"73456", "nombres"=>"JESUS ALBERTO", "apellidos" => "MARQUEZ ACEVEDO", 												"cedula" => "12502219"),
+									array("asignatura"=>"CS05", "codigo"=>"34234", "nombres"=>"DAVID ALEJANDRO", "apellidos" => "MARQUEZ OLASCOAGA", 												"cedula" => "1123789"));
+		return $this->render('CituaoCoordBundle:Default:practicantes.html.twig', array('listaPracticantes' => $listaPracticantes));
 	}
 	
 	
 	public function practicanteAction(){
 		
-		
-		return $this->render('CituaoCoordBundle:Default:practicante.html.twig', array("cedula"=>"12502219"));
+		$practicante = array("nombres"=>"JESUS ALBERTO", "apellidos" => "MARQUEZ ACEVEDO", "cedula" => "12502219");
+		return $this->render('CituaoCoordBundle:Default:practicante.html.twig', array('practicante'=>$practicante));
 	}
 
 	public function cronogramaAction(){
