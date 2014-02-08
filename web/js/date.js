6bab6b5fb2bb8fdc6feccc3af6dd1b3c0ccd09e3
jmarquez <jesmarquezacevedo@yahoo.es>
2014-02-08 11:53:09 -0500

    cronograma calculo de fechas

--- web/js/date.js
+++ web/js/date.js
@@ -1,33 +1,50 @@
+//
 $(document).ready(function(){
-	var date1 = new Date;
-	date1.setHours(0, 0, 0, 0);
-	date1.setDate(10);
-	var date2 = new Date;
-	date2.setHours(0, 0, 0, 0);
-	date2.setDate(23);
 
-	
-			$(function() {
-			$("#datepicker").datepicker({
-				beforeShowDay: function(date) {
-					var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
-					var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
-					return [true, date1 && ((date.getTime() == date1.getTime()) || (date2 && date >= date1 && date <= date2)) ? "dp-highlight" : ""];
-				},
-				onSelect: function(dateText, inst) {
-					var date1 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input1").val());
-					var date2 = $.datepicker.parseDate($.datepicker._defaults.dateFormat, $("#input2").val());
-					if (!date1 || date2) {
-						$("#input1").val(dateText);
-						$("#input2").val("");
-						$(this).datepicker("option", "minDate", dateText);
-					} else {
-						$("#input2").val(dateText);
-						$(this).datepicker("option", "minDate", null);
-					}
-				}
-			});
+	$(function() {
+		$("#datepicker").datepicker({
+			dateFormat: "dd-mm-yy",
+			onSelect: function(dateText, inst) {
+				var fecha_inicio = $.datepicker.parseDate(inst.settings.dateFormat || $.datepicker._defaults.dateFormat, dateText, inst.settings);
+				
+				//fecha para la primera asesoría tiempo estimado 7 días despues de iniciar el cronograma
+				fecha_inicio.setDate(fecha_inicio.getDate() + 7);
+				var fecha_asesoria1 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+				//calcular la asesoría #2 tiempo estimado 21
+				fecha_inicio.setDate(fecha_inicio.getDate() + 21);
+				var fecha_asesoria2 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+				//calcular la asesoría #3 tiempo estimado 30 días
+				fecha_inicio.setDate(fecha_inicio.getDate() + 30);
+				var fecha_asesoria3 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+				//calcular la asesoría #4 tiempo estimado 30 días
+				fecha_inicio.setDate(fecha_inicio.getDate() + 30);
+				var fecha_asesoria4 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+				//calcular la asesoría #5 tiempo estimado 30 días
+				fecha_inicio.setDate(fecha_inicio.getDate() + 30);
+				var fecha_asesoria5 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+				//calcular la asesoría #6 tiempo estimado 30 días
+				fecha_inicio.setDate(fecha_inicio.getDate() + 30);
+				var fecha_asesoria6 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+				//calcular la asesoría #7 tiempo estimado 30 días
+				fecha_inicio.setDate(fecha_inicio.getDate() + 30);
+				var fecha_asesoria7 = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+
+				//calcular la asesoría #7 tiempo estimado 5 días
+				fecha_inicio.setDate(fecha_inicio.getDate() + 5);
+				var fecha_finalizacion = $.datepicker.formatDate("dd-mm-yy", fecha_inicio, inst.settings);
+
+				//colocar las fechas en la tabla
+				$("#fecha_asesoria1").text(fecha_asesoria1);
+				$("#fecha_asesoria2").text(fecha_asesoria2);
+				$("#fecha_asesoria3").text(fecha_asesoria3);
+				$("#fecha_asesoria4").text(fecha_asesoria4);
+				$("#fecha_asesoria5").text(fecha_asesoria5);
+				$("#fecha_asesoria6").text(fecha_asesoria6);
+				$("#fecha_asesoria7").text(fecha_asesoria7);
+				$("#fecha_finalizacion").val(fecha_finalizacion);
+			}
 		});
+	});
 	
 });
 
