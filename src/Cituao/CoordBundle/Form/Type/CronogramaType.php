11a0768bb01a035d3fcb9c1733c5c27ed83bda80
Jesus Marquez <jesmarquezacevedo@yahoo.es>
2014-02-20 01:26:36 -0500

    relaciones practicantes y asessores

--- src/Cituao/CoordBundle/Form/Type/CronogramaType.php
+++ src/Cituao/CoordBundle/Form/Type/CronogramaType.php
@@ -6,21 +6,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class HojadevidaType extends AbstractType
+class CronogramaType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-	    ->add('codigo','text', array('label' => 'Codigo' , 'read_only'=>'true'))
+	    
         ->add('apellidos','text', array('label' => 'Apellidos', 'read_only'=>'true'))
 		->add('nombres','text', array('label' => 'Nombres','read_only'=>'true'))
-		->add('ci','text', array('label' => 'Cedula de Identidad','read_only'=>'true'))
 		
 		
-        ->add('emailInstitucional', 'email',  array('label' => 'Email institucional', 'read_only' => 'true'))
-        ->add('emailPersonal', 'email',  array('label' => 'Email personal', 'attr' => array('placeholder' => 'usuario@servidor')))
-		->add('telefonoMovil','text', array('label' => 'Teléfono móvil'));
 		
+		//->add('ci','text', array('label' => 'Cedula de Identidad','read_only'=>'true'))
+		//->add('codigo','text', array('label' => 'Codigo' , 'read_only'=>'true'))		
 		//->add('modalidad','choice', array('label' => 'Modalidad', 'choices'=> array('aud'=>'Audio', 'vis'=>'Visual', 'imp'=>'Impreso'),'multiple'=>true))
 		//->add('tipo','choice', array('label' => 'Tipo', 'choices'=> array('nac'=>'Nacional', 'int'=>'Internacional')))
     }
