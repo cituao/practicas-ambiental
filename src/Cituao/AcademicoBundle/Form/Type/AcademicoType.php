4296089de9bd72191f6b81bb9b0add81aec3386c
jamarquez <jesmarquezacevedo@yahoo.es>
2014-03-06 17:51:28 -0500

    agregue atributo categoria a asesor academico

--- src/Cituao/AcademicoBundle/Form/Type/AcademicoType.php
+++ src/Cituao/AcademicoBundle/Form/Type/AcademicoType.php
@@ -18,8 +18,8 @@ class AcademicoType extends AbstractType
         ->add('email', 'email',  array('label' => 'Email:',  'attr' => array('placeholder' => 'usuario@servidor'), 'required' => true ))
 		->add('telefonoMovil','text', array('label' => 'Teléfono móvil:', 'required' => true))
 		->add('telefonoFijo','text', array('label' => 'Teléfono fijo:'))
-		->add('perfil','textarea', array('label' => 'Perfil', 'max_length' => '500' ,  'attr' => array('placeholder' => 'Ingrese el perfil del asesor', 'cols' => '5', 'rows' => '5')));
-
+		->add('perfil','textarea', array('label' => 'Perfil', 'max_length' => '500' ,  'attr' => array('placeholder' => 'Ingrese el perfil del asesor', 'cols' => '5', 'rows' => '5')))
+		->add('categoria', 'choice', array('choices'=> array('a' => 'A', 'b' => 'B', 'c' => 'C', 'd' => 'D')));	
 		
 
 		//->add('modalidad','choice', array('label' => 'Modalidad', 'choices'=> array('aud'=>'Audio', 'vis'=>'Visual', 'imp'=>'Impreso'),'multiple'=>true));*/
