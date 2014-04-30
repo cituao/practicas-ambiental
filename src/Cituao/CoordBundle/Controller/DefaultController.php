2028dceffa0d522e4e8cd99c16ce6b9b4e953821
jamarquez <jesmarquezacevedo@yahoo.es>
2014-04-30 12:01:17 -0500

    update name file defaultpicture record new actors

--- src/Cituao/CoordBundle/Controller/DefaultController.php
+++ src/Cituao/CoordBundle/Controller/DefaultController.php
@@ -89,7 +89,7 @@ class DefaultController extends Controller
 		
         if ($formulario->isValid()) {
 			//si subio no subio foto  le asignamos una foto generica
-			if ($practicante->getFile() == NULL) 	$practicante->setPath('user.jpeg');
+			if ($practicante->getFile() == NULL) 	$practicante->setPath('defaultPicture.png');
 			//subimos la foto al servidor
 			$practicante->upload();
 
@@ -395,7 +395,7 @@ class DefaultController extends Controller
 
 					$practicante->setEstado(false);
 
-					$practicante->setPath('user.jpeg');
+					$practicante->setPath('defaultPicture.png');
 					$em->persist($practicante);
 					$em->flush();
 					$i++;
@@ -562,7 +562,7 @@ class DefaultController extends Controller
 				throw $this->createNotFoundException('¡La cédula ingresada ya existe!');
 			}
 
-			if ($academico->getFile() == NULL) 	$academico->setPath('user.jpeg');
+			if ($academico->getFile() == NULL) 	$academico->setPath('defaultPicture.png');
 			$academico->upload();	
 			
             // Completar las propiedades que el usuario no rellena en el formulario
